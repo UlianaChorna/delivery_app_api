d4979ec53db920bbb8458fff7f3e96b672c62c7f
UlianaChorna <110450537+UlianaChorna@users.noreply.github.com>
2023-05-26 20:42:52 +0300

    netlify integration

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const serverless = require('serverless-http')
+const app = express();
+const dotenv = require("dotenv")
+const mongoose = require('mongoose');
+const Shop = require("./functions/shop.js")
+const Product = require("./functions/product.js")
+
+const UserCart = require("./functions/userCart.js")
+
+dotenv.config()
+// mongoose.set('strictQuery', true);
+
+mongoose.connect(process.env.MONGO_URL, {
+   useNewUrlParser:true,
+   useUnifiedTopology:true,
+    
+}).then( ( ) => {
+console.log("DB Connection") 
+
+}).catch((err) => console.log(err))
+
+app.listen(8800, ()=> {
+    console.log("Backend server is running")
+})
+
+app.use(express.json());
+
+app.use("/api/shop", Shop);
+app.use("/api/product", Product);
+app.use("/api/cart", UserCart)
+
+module.exports.handler = serverless(app);
